a159ccce1ee89bdb647b4b9450f04ccacde0586d
Toha Islam <islamtoha710@gmail.com>
2022-10-27 10:15:35 +0600

    written something about java script

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ app.use(cors());
 const Port = process.env.Port || 5000;
 
 
+const courses = require('./data/coureses.json')
+
+
+app.get('/', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.id == id);
+    res.send(selectedCourse)
+})
+
 
 app.get("/", (req, res) => {
     res.send('Server is running')
